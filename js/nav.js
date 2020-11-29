c6c8cec55a6bf3382772e7b00e5a717f19cc1045
kibonga <66330623+kibonga@users.noreply.github.com>
2020-11-29 21:25:43 +0100

    Add files via upload
    Added Sections: Services, Banner-1, Programs(not finnished)

--- js/nav.js
+++ js/nav.js
@@ -1,92 +1,160 @@
+document.addEventListener('DOMContentLoaded', function() {
 
-// Nav
-loadNav();
-function loadNav() {
-    const nav = document.getElementById('nav');
-
-    const nizNav = new Array(
-        ['Home', 'index.html'],
-        ['About', 'about.html'],
-        ['Programs', 'programs.html'],
-        ['Author', 'author.html'],
-        ['Docs', 'docs.html']
-    );
+    // Global Load
+    loadNav();
+    loadHeader();
+    loadServices();
 
 
-    const logo = document.createElement('div');
-    const logoLink = document.createElement('a');
-    logoLink.textContent = `athleteON`;
-    logoLink.setAttribute('href', 'index.html');
-    logo.appendChild(logoLink);
-    logo.classList.add('nav__logo');
-    nav.appendChild(logo);
-    const ul = document.createElement('ul');
-    ul.classList.add('nav__menu')
+
+
+    // Nav
+    function loadNav() {
+        const nav = document.getElementById('nav');
     
-    for(let i=0; i<nizNav.length; i++) {
-        const li = document.createElement('li');
-        li.classList.add('nav__item');
-        const a = document.createElement('a');
-        a.textContent = nizNav[i][0];
-        a.setAttribute('href', nizNav[i][1]);
-        a.classList.add('nav__link')
-        li.appendChild(a);
-        ul.appendChild(li);
-    }
-    const hamburger = document.createElement('div');
-    hamburger.classList.add('nav__hamburger');
-    const i = document.createElement('i');
-    i.classList.add('fa', 'fa-bars', 'nav__icon');
-    i.setAttribute('aria-hidden', 'true');
-    hamburger.appendChild(i);
-    ul.appendChild(hamburger);
-    nav.appendChild(ul);
-
-
-    //Nav Mobile
-    const navMobile = document.getElementById('nav-mobile');
-    navMobile.classList.add('left');
-    const ulMobile = document.createElement('ul');
-    ulMobile.classList.add('nav-mobile__menu', 'mt-lg')
-    for(let i=0; i<nizNav.length; i++) {
-        const li = document.createElement('li');
-        li.classList.add('nav-mobile__item');
-        const a = document.createElement('a');
-        a.textContent = nizNav[i][0];
-        a.setAttribute('href', nizNav[i][1]);
-        a.classList.add('nav-mobile__link')
-        li.appendChild(a);
-        ulMobile.appendChild(li);
-    }
-    navMobile.appendChild(ulMobile);
-
-    const dark = document.getElementById('dark-overlay');
-
-
-    hamburger.addEventListener('click', (e) => {
-        e.stopPropagation();
-
-        navMobile.classList.toggle('left');
-        navMobile.classList.toggle('right');
-        darkOverlay();
-    })
-
-    // let dar = document.querySelector('body');
-    dark.addEventListener('click', (e) => {
-        e.stopPropagation();
-    // navMobile.classList.toggle('left');
-    // navMobile.classList.toggle('right');
+        const nizNav = new Array(
+            ['Home', 'index.html'],
+            ['About', 'about.html'],
+            ['Programs', 'programs.html'],
+            ['Author', 'author.html'],
+            ['Docs', 'docs.html']
+        );
     
-    if(!dark.classList.contains('d-none')) {
-        darkOverlay();
-        navMobile.classList.toggle('left');
-        navMobile.classList.toggle('right');
+    
+        const logo = document.createElement('div');
+        const logoLink = document.createElement('a');
+        logoLink.textContent = `athleteON`;
+        logoLink.setAttribute('href', 'index.html');
+        logo.appendChild(logoLink);
+        logo.classList.add('nav__logo');
+        nav.appendChild(logo);
+        const ul = document.createElement('ul');
+        ul.classList.add('nav__menu')
+        
+        for(let i=0; i<nizNav.length; i++) {
+            const li = document.createElement('li');
+            li.classList.add('nav__item');
+            const a = document.createElement('a');
+            a.textContent = nizNav[i][0];
+            a.setAttribute('href', nizNav[i][1]);
+            a.classList.add('nav__link')
+            li.appendChild(a);
+            ul.appendChild(li);
+        }
+        const hamburger = document.createElement('div');
+        hamburger.classList.add('nav__hamburger');
+        const span = document.createElement('span');
+        span.classList.add('material-icons', 'nav__icon');
+        span.setAttribute('aria-hidden', 'true');
+        span.textContent = 'menu';
+        hamburger.appendChild(span);
+        ul.appendChild(hamburger);
+        nav.appendChild(ul);
+    
+    
+        //Nav Mobile
+        const navMobile = document.getElementById('nav-mobile');
+        navMobile.classList.add('left');
+        const ulMobile = document.createElement('ul');
+        ulMobile.classList.add('nav-mobile__menu', 'mt-lg')
+        for(let i=0; i<nizNav.length; i++) {
+            const li = document.createElement('li');
+            li.classList.add('nav-mobile__item');
+            const a = document.createElement('a');
+            a.textContent = nizNav[i][0];
+            a.setAttribute('href', nizNav[i][1]);
+            a.classList.add('nav-mobile__link')
+            li.appendChild(a);
+            ulMobile.appendChild(li);
+        }
+        navMobile.appendChild(ulMobile);
+    
+        const dark = document.getElementById('dark-overlay');
+        hamburger.addEventListener('click', (e) => {
+            e.stopPropagation();
+    
+            navMobile.classList.toggle('left');
+            navMobile.classList.toggle('right');
+            darkOverlay();
+        })
+        dark.addEventListener('click', (e) => {
+            e.stopPropagation();
+        if(!dark.classList.contains('d-none')) {
+            darkOverlay();
+            navMobile.classList.toggle('left');
+            navMobile.classList.toggle('right');
+        }
+        })
+        function darkOverlay() {
+            dark.classList.toggle('d-none'); 
+        }
     }
-    })
 
-    function darkOverlay() {
-        dark.classList.toggle('d-none'); 
+
+
+    // Header
+    function loadHeader() {
+        const header = document.getElementById('header');
+        const headerContent = document.createElement('div');
+        headerContent.classList.add('header__content');
+        const h2 = document.createElement('h2');
+        h2.classList.add('heading-primarni', 'heading-primarni--dopunski');
+        h2.textContent = `where athletes are made`;
+        const h1 = document.createElement('h1');
+        h1.classList.add('heading-primarni', 'heading-primarni--glavni');
+        h1.textContent = `Turn Your athlete`;
+        const span = document.createElement('span');
+        span.textContent = `on`;
+        const btn = document.createElement('a');
+        btn.classList.add('btn');
+        btn.textContent = `Our Programs`;
+        btn.setAttribute('href', 'programs.html');
+
+        h1.appendChild(span);
+        headerContent.appendChild(h2);
+        headerContent.appendChild(h1);
+        headerContent.appendChild(btn);
+        header.appendChild(headerContent);
     }
 
-}
+
+    //Services 
+    function loadServices() {
+        const services = document.getElementById('services');
+        const serviceHeading = document.createElement('h3');
+        serviceHeading.classList.add('heading-sekundarni', 'services__heading');
+        serviceHeading.textContent = 'Our services';
+        const cards = document.createElement('div');
+        cards.classList.add('services__cards', 'mt-md');
+        services.appendChild(serviceHeading);
+
+        const nizCards = new Array(
+            ['supervisor_account','Worlds best coaches','Devlop Your athletic performance with the help of our excellent coaching Staff'],
+            ['fitness_center','Excellent Equipment','Devlop Your athletic performance with the help of our excellent coaching Staff'],
+            ['sports_mma','Training for any Sport','Devlop Your athletic performance with the help of our excellent coaching Staff'],
+            ['self_improvement','Injury Prevention','Devlop Your athletic performance with the help of our excellent coaching Staff'],
+        );
+        for(let i=0; i<nizCards.length; i++) {
+            const card = document.createElement('div');
+            card.classList.add('services__card');
+            const span = document.createElement('span');
+            span.classList.add('material-icons', 'services__icon');
+            span.textContent = nizCards[i][0];
+            const h4 = document.createElement('h4');
+            h4.classList.add('heading-ternarni', 'services__card-heading');
+            h4.textContent = nizCards[i][1];
+            const p = document.createElement('p');
+            p.textContent = nizCards[i][2];
+
+            card.appendChild(span);
+            card.appendChild(h4);
+            card.appendChild(p);
+            cards.appendChild(card);
+        }
+        services.appendChild(cards);
+    }
+})
+
+
+
 
